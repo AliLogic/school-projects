0ababfc4fbb68b693a397fdbfb3025b0fe154593
AliLogic <22447370+AliLogic@users.noreply.github.com>
2020-11-15 14:50:29 +0500

    complete circle project

--- circle.cpp
+++ circle.cpp
@@ -10,5 +10,22 @@ const double Pi = 3.141592;
 
 int main()
 {
+	double
+		circleRadius,
+		circleArea,
+		circleCircumference
+	;
+
 	std::cout << "Circle Circumference and Area Calculator!" << std::endl;
+
+	std::cout << "Insert value of circle radius: ";
+	std::cin >> circleRadius;
+
+	circleArea = Pi * (circleRadius * circleRadius);
+	std::cout << std::endl << "Area of the circle: " << circleArea;
+
+	circleCircumference = 2 * Pi * circleRadius;
+	std::cout << std::endl << "Circumference of the circle: " << circleCircumference;
+
+	return 0;
 }
