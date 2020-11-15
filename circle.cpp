b045e90887b6aed6e57a74be6e2b89feba49d89b
AliLogic <22447370+AliLogic@users.noreply.github.com>
2020-11-15 14:09:00 +0500

    Create circle.cpp
    WIP

---
+++ circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+const double Pi = 3.141592;
+
+/*
+	some nice information about the constant Pi
+	http://www.math.com/tables/constants/pi.htm
+*/
+
+int main()
+{
+	std::cout << "Circle Circumference and Area Calculator!" << std::endl;
+}
